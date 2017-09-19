418b8bfe1b78a188246e8a6ee5338c81ea161a4d
Elisei Timofte <timofte@unfuddle.com>
2017-09-19 17:51:11 +0300

    setup the db

--- mongo-bug/server/main.js
+++ mongo-bug/server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+Elements = new Mongo.Collection("elements");
 
 Meteor.startup(() => {
+  Elements.insert({_id: '1UniqIdForMyTest', title: 'first', _items: [{value: 'started', type: '%'}] });
   // code to run on server at startup
 });
